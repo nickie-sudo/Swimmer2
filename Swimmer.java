4d8ba0ffd84ae11d7db06fd8d27e1a8cf0e7d95e
Nick Marino <65463649+nickie-sudo@users.noreply.github.com>
2022-12-12 13:19:26 -0500

    Add files via upload

---
+++ Swimmer.java
@@ -0,0 +1,19 @@
+
+/**
+ * Abstract class Swimmer - write a description of the class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Swimmer
+{
+    // instance variables - replace the example below with your own
+    private double height;
+    private int age;
+    private int skillLevel;
+    private String name;
+
+    public void hey()
+    {
+    }
+}
