9cc1aaf3946b6afc78887d4f983883c27e9b81d4
AlexLowczyk <106122867+AlexLowczyk@users.noreply.github.com>
2022-12-12 13:22:35 -0500

    Update Swimmer.java

--- Swimmer.java
+++ Swimmer.java
@@ -1,19 +1,36 @@
-
-/**
- * Abstract class Swimmer - write a description of the class here
- *
- * @author Nicholas Marino and Alex Lowczyk
- * @version (version number or date here)
- */
 public abstract class Swimmer
 {
     // instance variables - replace the example below with your own
-    private double height;
+    private int height;
     private int age;
-    private int skillLevel;
+    private String skillLevel;
     private String name;
 
-    public void hey()
+    /**
+     * Constructor for objects of class Swimmer
+     */
+    public Swimmer()
     {
+        // initialise instance variables
+        name = "Alex Lowczyk";
+        height = 72;
+        age = 17;
+        skillLevel = "intermediate";
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getAge()
+    {
+        // put your code here
+        return age;
+    }
+    public String getSkillLevel() 
+    {
+      return skillLevel;  
     }
 }
