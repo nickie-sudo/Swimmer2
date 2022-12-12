00aac90df6610f449b35e48be5bac5172db73124
Nick Marino <65463649+nickie-sudo@users.noreply.github.com>
2022-12-12 13:23:34 -0500

    Update Swimmer.java

--- Swimmer.java
+++ Swimmer.java
@@ -1,3 +1,10 @@
+/**
+ * Abstract class Swimmer - write a description of the class here
+ *
+ * @author Nicholas Marino and Alex Lowczyk
+ * @version (version number or date here)
+ */
+
 public abstract class Swimmer
 {
     // instance variables - replace the example below with your own
