2db5995e92831307ef919ca129cb3578edf21517
Nick Marino <65463649+nickie-sudo@users.noreply.github.com>
2022-12-20 13:09:06 -0500

    Update FiftyFree.java

--- Freestyle/FiftyFree.java
+++ Freestyle/FiftyFree.java
@@ -1,1 +1,60 @@
+public class FifityFree
+{
+    private double personalBest;
+    private double firstFiftyTime;
+    private double lastFiftyTime;
+    private int amtOfPeopleCheering;
 
+    public FifityFree()
+    {
+        personalBest = 25.6 ;
+        firstFiftyTime = 30.5 ;
+        lastFiftyTime =  32.6;
+        amtOfPeopleCheering = 2019765709;
+    }
+
+    public FifityFree(double personal, double first, double last, int amt)
+    {
+        personalBest = personal;
+        firstFiftyTime = first;
+        lastFiftyTime =  last;
+        amtOfPeopleCheering = amt;
+    }
+
+    public void increaseCheer()
+    {
+        amtOfPeopleCheering +=1;
+    }
+
+    public void setPersonalBest(double newPersonalBest)
+    {
+        personalBest = newPersonalBest;
+    }
+
+    public void resetTimes()
+    {
+        firstFiftyTime = 0;
+        lastFiftyTime = 0;
+    }
+
+    public boolean equals()
+    {
+        double num = .000001;
+        if((firstFiftyTime - lastFiftyTime) > num){
+            return true;
+        }
+        else { 
+            return false;
+        }
+    }
+
+    public String toString()
+    {
+        return "Personal Best: " + personalBest + "First Fifty Free: " + firstFiftyTime + "Last Fifty Free: " + lastFiftyTime;
+    }
+
+    public int compareTo(FifityFree free)
+    {
+        return 1;
+    }
+}
